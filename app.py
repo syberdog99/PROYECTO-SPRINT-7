ab94af64ec7c4746318cdc897a32b1e22f851fb6
syberdog99 <trexcamarasa@gmail.com>
2026-08-16 20:45:12 -0600

    proyecto reparacion v2

--- app.py
+++ app.py
@@ -14,5 +14,7 @@ if disp_button:
     st.write("Creacion de un grafico de dispersion para el conjunto de datos de anuncios de venta de coches")
     fig = go.Figure(data=[go.Scatter(x=car_data["odometer"], y=car_data["price"], mode="markers")])
     fig.update_layout(title_text="Relacion entre Odometro y Precio")
+    st.plotly_chart(fig, use_container_width=True)
+
     fig.show()
     
