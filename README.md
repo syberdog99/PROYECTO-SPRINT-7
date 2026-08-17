4356a8bf670ba6fa2b7f7b0e97e222d8652ddd28
syberdog99 <trexcamarasa@gmail.com>
2026-08-16 20:34:38 -0600

    proyectosprint7

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # PROYECTO-SPRINT-7
 ESTE SERA EL PROYECTO DEL SPRINT 7
+en este proyecto, se creo una aplicacion con dos botones para ver un histograma y un grafico de dispersion, el histograma sirve para ver como se distribuye una variable numerica, por ejemplo el kilometraje de los coches y el de dsipersion sirve para ver la relacion de dos variables numericas
--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st 
 import pandas as pd 
 import plotly.graph_objects as go
-car_data = pd.read.csv("vehicles_us.csv")
+car_data = pd.read_csv("vehicles_us.csv")
 st.header("Analisis de anuncios de venta de coches")
 hist_button = st.button("construir histograma")
 if hist_button:
@@ -9,3 +9,10 @@ if hist_button:
     fig = go.Figure(data=[go.Histogram(x=car_data["odometer"])])
     fig.update_layout(title_text="Distribucion del Odometro")
     st.plotly_chart(fig, use_container_width=True)
+disp_button = st.button("construir grafico de dispersion")
+if disp_button:
+    st.write("Creacion de un grafico de dispersion para el conjunto de datos de anuncios de venta de coches")
+    fig = go.Figure(data=[go.Scatter(x=car_data["odometer"], y=car_data["price"], mode="markers")])
+    fig.update_layout(title_text="Relacion entre Odometro y Precio")
+    fig.show()
+    
